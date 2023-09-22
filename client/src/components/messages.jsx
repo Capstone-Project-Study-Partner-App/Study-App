dcf749e8a3e43bc36a8cfa3ee45ccac72335870b
anagabrieleloor <anagabrieleloor@outlook.com>
2023-09-22 16:39:04 -0400

    all user messages rendered

--- client/src/components/messages.jsx
+++ client/src/components/messages.jsx
@@ -1,8 +1,66 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getUserMessages } from "../../fetching";
 
-export default function Messages (){
+
+export default function AllMessages() {
+    const [messages, setMessages] = useState([]);
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState("");
+    const [searchParam, setSearchParam] = useState("");
+  
+    useEffect(() => {
+      async function getAllMessages() {
+        try {
+          const response = await getUserMessages(11);
+          setMessages(response);
+        } catch (error) {
+          setError(error.message);
+        }
+      }
+      getAllMessages();
+    }, []);
+  
+    const messagesToDisplay = searchParam
+      ? messages.filter((message) =>
+          message.receiver_first_name.toLowerCase().includes(searchParam)
+        )
+      : messages;
+  
     return (
-        <div>
-            <h1>This is messages page</h1>
+      <div className="match-container">
+        <div className="match-card">
+          <h3 className="all-messages-banner">messages:</h3>
+          <Link to="/messages/new">send a message</Link>
+  
+  
+  
+          {messagesToDisplay.map((message) => (
+            <div key={message.message_id}>
+              <h4>{message.receiver_first_name}</h4>
+              <p>
+                <Link to={`/thread/${message.thread_id}`}>
+                  <img src={message.receiver_photo} id="user-profile-image" style={{width: '100px'}} />
+                </Link>
+              </p>
+              <p>
+                <b>{message.receiver_first_name}:</b> {message.message_content}
+              </p>
+              {/* <DeleteMessage message_id={message.message_id} /> */}
+              <Link to={`/messages/edit/${message.message_id}`}>edit</Link>
+              <hr className="rounded" />
+            </div>
+          ))}
         </div>
-    )
-}
+        <label>
+            ♡{" "}
+            <input
+              type="text"
+              placeholder="search by user"
+              value={searchParam}
+              onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
+            />
+          </label>
+      </div>
+    );
+  }
