75e67aa6bdbcfc8596d4af3c915f73064745e0a5
kaykyolee <kleegoma@gmail.com>
2023-09-21 21:41:22 -0400

    initial navbar and links set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import './App.css';
+import Navbar from './components/navbar';
+import Home from './components/home';
+import Buddies from './components/buddies';
+import Events from './components/events';
+import Messages from './components/messages';
+import Profile from './components/profile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div id='nav-bar-container'>
+        <Navbar/> 
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+          <Route path='/' element={<Home/>}/> 
+          <Route path='/users' element={<Buddies/>}/>
+          <Route path='/events' element={<Events/>}/>
+          <Route path='/:id/messages' element={<Messages/>}/>
+          <Route path='/users/:id' element={<Profile/>}/>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom';
+
+
+export default function Navbar (){
+    return (
+        <div>
+            <Link to='/'>Home</Link>
+            <Link to='/users'>Buddies</Link>
+            <Link to='/events'>Events</Link>
+            <Link to='/:id/messages'>Messages</Link>
+            <Link to='/users/:id'>Profile</Link>
+        </div>
+    )
+
+}
