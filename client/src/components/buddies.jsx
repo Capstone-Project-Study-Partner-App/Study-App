dbe4411ab5d10ebe3949e858f09e67e471efe484
Cassie Roan <cassandracroan@gmail.com>
2023-09-26 11:20:43 -0400

    removed empty spaces

--- client/src/components/buddies.jsx
+++ client/src/components/buddies.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getAllUsers } from '../fetching';
 import { Link } from 'react-router-dom'; // Import Link
-
-
 export default function Buddies(){
     const [allUsers, setAllUsers] = useState([]);
   useEffect(() => {
@@ -13,9 +11,9 @@ export default function Buddies(){
     fetchData();
   }, []);
 
-
     return (
         <div className="flex">
+        
         {/* Sidebar */}
         <div className="w-1/4 bg-gray-800 text-white p-4">
             <h1 className="text-xxl font-semibold mb-4">Filters</h1>
@@ -222,39 +220,6 @@ export default function Buddies(){
         </div>
   );
 }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    //     <div>
-    //   <h1 style={{ textAlign: 'center' }}>All Buddies</h1>
-    //   <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-    //     {allUsers ? (
-    //       allUsers.map(({ user_id, photo, first_name, last_name, gender, dob, education_level }) => (
-    //         <Link key={user_id} to={`/users/${user_id}`}>
-    //           <div key={user_id}>
-    //             <img src={photo} />
-    //             <p>{first_name} {last_name}</p>
-    //             <p>Gender: {gender}</p>
-    //             <p>Age: {dob}</p>
-    //             <p>Level of Education: {education_level}</p>
-    //           </div>
-    //         </Link>
-    //       ))
-    //     ) : (
-    //       <p>No buddies available.</p>
-    //     )}
-    //   </div>
-    // </div>
-
-
 
 Buddies.propTypes = {
 };
