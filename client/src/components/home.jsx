7d9d7b732842875f493762291e7cd2eb65695f2b
anagabrieleloor <anagabrieleloor@outlook.com>
2023-09-29 12:03:38 -0400

    updating message layout and testing homepage layout

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,35 +1,91 @@
 
 
 export default function Home() {
-    return (
-      <div className="home-container">
-        <div className="home-image">
-          <img
-            src="https://i.pinimg.com/1200x/35/a0/d9/35a0d99126faaca0d33305bd1a86ee20.jpg"
-            alt="Placeholder Image"
-            style={{width: '500px'}}
-          />
-        </div>
-  
-        <div className="login-form">
-          <h1>Login</h1>
-          <form action="">
-            <label htmlFor="username">Username</label>
-            <input type="text" />
-            <br />
-            <label htmlFor="password">Password</label>
-            <input type="password" />
-            <br />
-            <input type="checkbox" />
-            <label>Remember Me</label>
-            <br />
-            <a href="#">Forgot Password?</a>
-            <br />
-            <button type="submit">Login</button>
-            <br />
-          </form>
-          <a href="#">Sign up Here</a>
+
+  return (
+    <div className="bg-gray-500 w-full">
+      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+            aria-hidden="true"
+          >
+            <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
+            <defs>
+              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stopColor="#7775D6" />
+                <stop offset={1} stopColor="#E935C1" />
+              </radialGradient>
+            </defs>
+          </svg>
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Boost your productivity.
+              <br />
+              Start using our app today.
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+              Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <a
+                href="#"
+                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-sm font-semibold leading-6 text-white">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+          <div className="relative mt-16 h-80 lg:mt-8">
+            <img
+              className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+              src="https://prod-website-cdn.studysmarter.de/sites/5/us/Study-with-Me-dark-2048x1152-1-1536x864-min.png"
+              alt="App screenshot"
+              width={1824}
+              height={1080}
+            />
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
+}
+
+
+
+  //   return (
+  //     <div className="home-container">
+  //       <div className="home-image">
+  //         <img
+  //           src="https://i.pinimg.com/1200x/35/a0/d9/35a0d99126faaca0d33305bd1a86ee20.jpg"
+  //           alt="Placeholder Image"
+  //           style={{width: '500px'}}
+  //         />
+  //       </div>
+  
+  //       <div className="login-form">
+  //         <h1>Login</h1>
+  //         <form action="">
+  //           <label htmlFor="username">Username</label>
+  //           <input type="text" />
+  //           <br />
+  //           <label htmlFor="password">Password</label>
+  //           <input type="password" />
+  //           <br />
+  //           <input type="checkbox" />
+  //           <label>Remember Me</label>
+  //           <br />
+  //           <a href="#">Forgot Password?</a>
+  //           <br />
+  //           <button type="submit">Login</button>
+  //           <br />
+  //         </form>
+  //         <a href="#">Sign up Here</a>
+  //       </div>
+  //     </div>
+  //   );
+  // }
