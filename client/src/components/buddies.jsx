98fee23c956992dc29620285e97a7b40825c97f4
Cassie Roan <cassandracroan@gmail.com>
2023-10-03 00:02:56 -0400

    styling touchups

--- client/src/components/buddies.jsx
+++ client/src/components/buddies.jsx
@@ -253,7 +253,7 @@ export default function Buddies() {
               className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow-md"
             >
               <div className="flex flex-1 flex-col p-8 bg-slate-50">
-                <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src={user.photo} alt={`${user.first_name} ${user.last_name}`} />
+                <img className="mx-auto w-32 aspect-square flex-shrink-0 rounded-full" src={user.photo} alt={`${user.first_name} ${user.last_name}`} />
                 <h3 className="mt-6 text-md font-medium text-gray-900">{user.first_name} {user.last_name}</h3>
                 <dl className="mt-1 flex flex-grow flex-col">
                   <dd className="text-sm text-gray-500"><b>Age:</b> {user.age}</dd>
@@ -270,7 +270,7 @@ export default function Buddies() {
               <div className="-mt-px flex divide-x divide-gray-200">
               <div className="flex w-0 flex-1">
                 <a href="#"
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg bg-indigo-800	 border border-transparent py-4 text-sm font-semibold text-white"
+                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-b-lg bg-indigo-800	 border border-transparent py-4 text-sm font-semibold text-white"
                 >
                   Connect
                 </a>
