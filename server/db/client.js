3760c7d23448d3ecbdc934464f1981a3e1b3d320
Cassie Roan <cassandracroan@gmail.com>
2023-10-10 23:42:31 -0400

    deploy (try again)

--- server/db/client.js
+++ server/db/client.js
@@ -1,10 +1,11 @@
 // Require Client from pg
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
 //Establishing connect to database (like how we do with http://)
-const client = new Client(
-  process.env.POSTGRES_DB_URL || `postgres://localhost:5432/study-app`
-);
+const client = new Pool({
+  connectionString:
+    process.env.POSTGRES_DB_URL || `postgres://localhost:5432/study-app`,
+});
 client.connect();
 
 //Export for use in other files
