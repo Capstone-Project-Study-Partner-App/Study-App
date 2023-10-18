88e59cda9d258a33b8585f3ae5ee3333a90958cd
kaykyolee <kleegoma@gmail.com>
2023-10-17 21:14:13 -0400

    postgres internal database url update

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,9 @@ const { Client } = require("pg");
 
 //Establishing connect to database (like how we do with http://)
 const client = new Client(
-  process.env.POSTGRES_DB_URL || `postgres://localhost:5432/study-app`
+  process.env.POSTGRES_DB_URL || 
+  // `postgres://localhost:5432/study-app`
+  `postgres://study_app_user:IIp71YfoxgIQZBNHOLAxgERTkWy3NeVX@dpg-cknrlmj1a5fs73b54sa0-a/study_app`
 );
 client.connect();
 
