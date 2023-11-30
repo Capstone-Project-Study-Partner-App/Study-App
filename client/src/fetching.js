5efba3dbf7d349d3f8e6a424ddcffa2585e86fd8
KaylaTan91 <kaylatan91@gmail.com>
2023-11-30 12:06:59 -0500

    frontend deployment

--- client/src/fetching.js
+++ client/src/fetching.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-undef
 const api_root = import.meta.env.VITE_API_BASE || 
-"http://localhost:8080/api";
+"https://binderbuddies-v8dl.onrender.com" ;
+// "http://localhost:8080/api";
 // "https://binder-buddies.onrender.com";
 
 (() => {
