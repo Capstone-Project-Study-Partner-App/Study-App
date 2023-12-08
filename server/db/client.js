b32a655e341da79ba1bd4660426df8c61d6da760
KaylaTan91 <kaylatan91@gmail.com>
2023-12-08 13:08:01 -0500

    removed extra client

--- server/db/client.js
+++ server/db/client.js
@@ -4,15 +4,14 @@ const { Client } = require("pg");
 //Establishing connect to database (like how we do with http://)
 const client = new Client(
   `postgres://binderbuddies_db_user:bYqjrV6QHSgJxJmvqOwnVKbrdn286pvw@dpg-clpkbmhoh6hc73c5lkfg-a/binderbuddies_db`
-
   // `postgres://localhost:5432/study-app`
   // `postgres://study_app_user:IIp71YfoxgIQZBNHOLAxgERTkWy3NeVX@dpg-cknrlmj1a5fs73b54sa0-a/study_app`
 );
-client.connect();
 
 //Export for use in other files
 module.exports = client;
 
 
+// client.connect();
 
 // process.env.POSTGRES_DB_URL || 
